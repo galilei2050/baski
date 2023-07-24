98cbe7ed86bc1de335f2a9e4cb332a53437f8265
galilei <galilei.mail@gmail.com>
2023-07-23 21:43:12 -0700

    Better naming

--- baski/infra/telegram_attribution/__init__.py
+++ baski/infra/telegram_attribution/__init__.py
@@ -2,7 +2,7 @@ from .. import pubsub, bigquery
 
 
 def setup(dataset_id):
-    # bigquery.load_dataset(dataset_id)
-    # bigquery.load_tables(dataset_id, __file__, "bigquery")
-    # pubsub.load_schemas(root=__file__, path='pubsub/schema')
+    bigquery.load_dataset(dataset_id)
+    bigquery.load_tables(dataset_id, __file__, "bigquery")
+    pubsub.load_schemas(root=__file__, path='pubsub/schema')
     pubsub.load_topics(root=__file__, path='pubsub/topic')
