464c01a0f882d479a94d0e5d99064028e534ac99
galilei <galilei.mail@gmail.com>
2024-02-01 18:02:47 -0800

    Slightly improve schema import

--- baski/schema/__init__.py
+++ baski/schema/__init__.py
@@ -1,4 +1,4 @@
-from marshmallow.fields import Boolean, String, DateTime, Integer, Float, Number, Decimal, UUID, Time, Date, TimeDelta, Dict
+from marshmallow.fields import Boolean, String, DateTime, Integer, Float, Number, Decimal, UUID, Time, Date, TimeDelta, Dict, List
 from marshmallow import ValidationError
 from .fields import *
 from .schema import *
